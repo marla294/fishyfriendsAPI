644f712932b907859c39d67b2ac9e617fb22f681
Marla Foreman <marla294@gmail.com>
2018-09-14 16:04:15 -0500

    First commit

---
+++ fishfriends.Biz/Database/ConnectionUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using Npgsql;
+
+
+namespace fishfriends.Biz.Database
+{
+    public class ConnectionUtils
+    {
+        static readonly string connString = "Host=127.0.0.1;Port=5433;Username=postgres;Password=Password1;Database=fishfriends";
+
+        readonly NpgsqlConnection conn = new NpgsqlConnection(connString);
+
+        public void OpenConnection()
+        {
+            conn.Open();
+        }
+
+        public void CloseConnection()
+        {
+            conn.Close();
+        }
+
+        public string RunCommand(string command)
+        {
+            var cmd = new NpgsqlCommand(command, conn);
+            var reader = cmd.ExecuteReader();
+
+            string str = "";
+
+            while (reader.Read())
+            {
+                str = reader.GetString(0);
+            }
+
+            return str;
+        }
+    }
+}
---
+++ fishfriends.Biz/Models/Fish.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace fishfriends.Biz.Models
+{
+    public class Fish
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
