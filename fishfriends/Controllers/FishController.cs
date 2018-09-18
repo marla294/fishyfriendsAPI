2bf530a3fba4eea06d3dc35250c52c1d5628023d
Marla Foreman <marla294@gmail.com>
2018-09-18 16:35:58 -0500

    Getting a very basic fish controller set up

---
+++ fishfriends/Controllers/FishController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using fishfriends.Biz.Models;
+
+namespace fishfriends.Service.Controllers
+{
+    public class FishController : ApiController
+    {
+        // Get All
+        public List<Fish> Get()
+        {
+            var fishList = new List<Fish>() { new Fish() { Name = "fred" } };
+
+            return fishList;
+        }
+    }
+}
