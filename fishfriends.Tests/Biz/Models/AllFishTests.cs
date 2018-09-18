c09e2457d5aefff9282a87939772326219c36c5a
Marla Foreman <marla294@gmail.com>
2018-09-18 17:28:32 -0500

    Making a small test to figure out what is getting returned from PostgreSQL

---
+++ fishfriends.Tests/Biz/Models/AllFishTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using fishfriends.Biz.Models;
+using fishfriends.Biz.Database;
+using NUnit.Framework;
+
+namespace fishfriends.Tests.Biz.Models
+{
+    [TestFixture]
+    public class AllFishTests
+    {
+        [Test]
+        public void TestAllFish()
+        {
+            var dB = new ConnectionUtils();
+
+            var name = dB.RunCommand("select name from fish;");
+
+            Assert.AreEqual(name, 1);
+        }
+    }
+}
