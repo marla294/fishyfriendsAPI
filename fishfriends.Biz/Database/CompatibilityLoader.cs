dc95cda7dc9f8262a2a3fb215784bd6ef1bf19be
Marla Foreman <marla294@gmail.com>
2018-09-19 14:35:04 -0500

    Created CompatibilityLoader class

---
+++ fishfriends.Biz/Database/CompatibilityLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using fishfriends.Biz.Models;
+
+namespace fishfriends.Biz.Database
+{
+    public class CompatibilityLoader
+    {
+        public CompatibilityLoader()
+        {
+        }
+    }
+}
--- fishfriends.Biz/Database/FishLoader.cs
+++ fishfriends.Biz/Database/FishLoader.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using fishfriends.Biz.Models;
-using Npgsql;
 
 namespace fishfriends.Biz.Database
 {
