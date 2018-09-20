0ecdf4e79b455c134fb2dcf8bd03d55fc45c4525
Marla Foreman <marla294@gmail.com>
2018-09-20 10:15:13 -0500

    Creating CompatibilityChecker class
    Adding GetCompatibility for two fish

---
+++ fishfriends.Biz/Database/CompatibilityChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using fishfriends.Biz.Models;
+
+namespace fishfriends.Biz.Database
+{
+    public class CompatibilityChecker
+    {
+        //How compatible the fish are, 0 - 10
+        //0 - bad
+        //10 - good
+        public int Compatible { get; private set; }
+     
+        public void GetCompatibility(Fish fishone, Fish fishtwo)
+        {
+            var dB = new ConnectionUtils();
+
+            var compatibilityResultSet = dB.GetResultSet("select c.compatible from compatibility c inner join fish f1 on c.fishone = f1.id inner join fish f2 on c.fishtwo = f2.id where f1.name = 'anthias' and f2.name = 'anthias'; ");
+        }
+    }
+}
