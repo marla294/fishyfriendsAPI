79d9ca11e1a3d564a168d8b41dfa44f5c566c30c
Marla Foreman <marla294@gmail.com>
2018-09-21 12:00:54 -0500

    Created FishCrafter class
    - So you can create a single fish or a list of fish with the same ID as
    in the database

---
+++ fishfriends.Biz/Models/FishCrafter.cs
@@ -0,0 +1,36 @@
+﻿using fishfriends.Biz.Database;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fishfriends.Biz.Models
+{
+    //Returns fish with the same fish ids in the database
+    public class FishCrafter
+    {
+        List<Fish> DbFishList { get; set; }
+
+        public FishCrafter()
+        {
+            DbFishList = new FishLoader().FishList;
+        }
+
+        public Fish CraftSingleFish(string name)
+        {
+            var fishId = DbFishList.FirstOrDefault(fish => fish.Name == name).Id;
+
+            return new Fish(fishId, name);
+        }
+
+        public List<Fish> CraftListOfFish(List<string> names)
+        {
+            List<Fish> FishList = new List<Fish>();
+
+            foreach (var name in names)
+            {
+                FishList.Add(CraftSingleFish(name));
+            }
+
+            return FishList;
+        }
+    }
+}
