9b90383d3e1e62fb3f4e7de4025b42791c3eb292
Marla Foreman <marla294@gmail.com>
2018-09-28 10:26:38 -0500

    Created FishInfoFactory to load fishInfo

---
+++ fishfriends.Biz/Database/FishInfoFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using fishfriends.Biz.Models;
+using System.Linq;
+
+namespace fishfriends.Biz.Database
+{
+    public static class FishInfoFactory
+    {
+
+    }
+}
