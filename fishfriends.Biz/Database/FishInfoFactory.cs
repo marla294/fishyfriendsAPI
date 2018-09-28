a5d2dd6f04f2ae3c8c92fba4366d6e4fce439435
Marla Foreman <marla294@gmail.com>
2018-09-28 11:01:33 -0500

    Getting FishInfoFactory set up along with tests
    Not done yet!

--- fishfriends.Biz/Database/FishInfoFactory.cs
+++ fishfriends.Biz/Database/FishInfoFactory.cs
@@ -7,6 +7,20 @@ namespace fishfriends.Biz.Database
 {
     public static class FishInfoFactory
     {
+        public static FishInfo LoadInfoSingleFish(string fishName)
+        {
+            var fishInfo = new FishInfo(fishName);
 
+            var sql = String.Format("select info from fishinfo where fish = {0} order by infoid;", fishInfo.Fish.Id);
+
+            var infoResultSet = ConnectionUtils.ExecuteCommand(new PostgreSQLConnection(), sql);
+
+            for (var i = 0; i < infoResultSet[0].Count; i++)
+            {
+                fishInfo.Info.Add(infoResultSet[0][i]);
+            }
+
+            return fishInfo;
+        }
     }
 }
---
+++ fishfriends.Tests/Biz/Database/FishInfoFactory.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using fishfriends.Biz.Database;
+
+namespace fishfriends.Tests.Biz.Database
+{
+    [TestFixture]
+    public class FishInfoFactoryTests
+    {
+        [Test]
+        public void TestFishInfoLoadSingle()
+        {
+            var testFishInfo = FishInfoFactory.LoadInfoSingleFish("blennies");
+
+            Assert.AreEqual(testFishInfo.Fish.Name, "blennies");
+            Assert.AreEqual(testFishInfo.Info.Count, 3);
+        }
+    }
+}
