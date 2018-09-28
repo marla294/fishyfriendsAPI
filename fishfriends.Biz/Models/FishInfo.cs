0599b47622a7de753511b8db5ecc32c6d7c155d1
Marla Foreman <marla294@gmail.com>
2018-09-28 10:16:44 -0500

    Add FishInfo class

---
+++ fishfriends.Biz/Models/FishInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using fishfriends.Biz.Database;
+
+namespace fishfriends.Biz.Models
+{
+    public class FishInfo
+    {
+        public Fish Fish { get; set; }
+        public List<string> Info { get; set; }
+
+        public FishInfo(string fishName)
+        {
+            Fish = FishFactory.LoadSingle(fishName);
+            Info = new List<string>();
+        }
+
+        public void AddInfo(string info)
+        {
+            Info.Add(info);
+        }
+    }
+}
