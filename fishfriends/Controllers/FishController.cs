57a44a557e236153b6df379132183b7fa34cb6e3
Marla Foreman <marla294@gmail.com>
2018-10-02 10:03:27 -0500

    Trying to enable cors, installed Microsoft.AspNet.WebApi.Cors package

--- fishfriends/Controllers/FishController.cs
+++ fishfriends/Controllers/FishController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using fishfriends.Biz.Database;
 using fishfriends.Biz.Models;
 
 namespace fishfriends.Service.Controllers
 {
+    [EnableCors("*", "*", "*")]
     public class FishController : ApiController
     {
         // Get All
